7e434418dce2e67770a9b362dbd563c5f5dc4a8e
Jibé Barth <barth.jib@gmail.com>
2019-03-31 12:06:26 +0200

    Require inky and cssinliner extension

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Twig\Inky\Bundle\TwigInkyBundle::class => ['all' => true],
+    Twig\CssInliner\Bundle\TwigCssInlinerBundle::class => ['all' => true],
 ];
